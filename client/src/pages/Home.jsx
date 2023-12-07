9d1b622423dc6fb987bc604e18762c16a79bc75c
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-12-07 01:32:54 +0100

    url server updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("https://dall-e-server-wpeh.onrender.com/api/v1/post", {
+      const response = await fetch("https://dall-e-server-f5qk.onrender.com/api/v1/post", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
